b7bad2dd304b463e19b551684d3bae9f9aeae325
jfkeci <ja.lip132@gmail.com>
2021-08-02 08:24:30 +0200

    database migrations

--- database/migrations/2021_08_02_061546_add_category_id_to_tasks.php
+++ database/migrations/2021_08_02_061546_add_category_id_to_tasks.php
@@ -14,7 +14,7 @@ class AddCategoryIdToTasks extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            //
+            $table->integer('category_id');
         });
     }
 
@@ -26,7 +26,7 @@ class AddCategoryIdToTasks extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            //
+            $table->dropColumn('category_id');
         });
     }
 }
