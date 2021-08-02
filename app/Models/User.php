619a577833336ff5f370aa25417d9cf2de0c5e1d
jfkeci <ja.lip132@gmail.com>
2021-08-02 08:26:47 +0200

    relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function tasks()
+    {
+        return $this->hasMany('App\Model\Task');
+    }
+    public function categories()
+    {
+        return $this->hasMany('App\Model\Category');
+    }
 }
